886974bb2f041fc0d23bb23c42552177bc899e50
oliwier <oliwierjaworski01@outlook.com>
2025-10-11 14:21:41 +0000

    added small user-test for checking if the correct reg is being used

--- apps/user_vdma_app/main.c
+++ apps/user_vdma_app/main.c
@@ -1,7 +1,9 @@
+#include <errno.h>
 #include <stdio.h>
 #include <stdint.h>
 #include <fcntl.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/mman.h>
 #include <sys/types.h>
 #include <unistd.h>
@@ -41,12 +43,13 @@
 
 #define log_error(str_arg)  \
     do {                    \
-        perror("str_arg")   \
+        fprintf(stderr,"Error in: %s, %s\n", str_arg, strerror(errno));\
         exit(1);            \
     }while(0)
 
 typedef struct{
     unsigned int baseAddr;
+    unsigned int pg_offset;
     int vdmaHandler;
     int width;
     int height;
@@ -62,24 +65,51 @@ typedef struct{
 
 }vdma_handle;
 
-int vdma_setup(vdma_handle* handle){
+int 
+vdma_setup(vdma_handle* handle){
     uint width = 1920, height = 1080, depth = 3;
+    int pg_offset=0;
+
     handle->baseAddr = DEV_VDMA_ADDR;
     handle->width = width;
     handle->height = height;
     handle->pixelDepth = depth;
     handle->fbLenght = width * height * depth;
     handle->vdmaHandler = open("/dev/mem", O_RDWR | O_SYNC);
-    handle->vdmaVirtAddr = (uintptr_t) mmap(NULL, DEV_VDMA_SIZE,
-                PROT_READ | PROT_WRITE, MAP_SHARED, handle->vdmaHandler, (off_t) handle->baseAddr);
-    if( handle->vdmaVirtAddr == MAP_FAILED )
-            write_error("mmap");
+    if( handle->vdmaHandler == -1 )
+        log_error("open");
+    
+    //get page unaligned memory (___ & 0xFFF) ps. it is already aligned just a check for later maybe or good to know 
+    pg_offset = handle->baseAddr & (sysconf(_SC_PAGE_SIZE) -1);
+    handle->pg_offset = pg_offset;
+    handle->vdmaVirtAddr = (uintptr_t) mmap(NULL, DEV_VDMA_SIZE + pg_offset,
+            PROT_READ | PROT_WRITE, MAP_SHARED, handle->vdmaHandler, (off_t) handle->baseAddr - pg_offset);
+    if( (void*) handle->vdmaVirtAddr == MAP_FAILED )
+        log_error("mmap");
+
+}
+
+int 
+vdma_remove(vdma_handle* handle){
+    munmap((void*)handle->vdmaVirtAddr, DEV_VDMA_SIZE + handle->pg_offset);
+    close(handle->vdmaHandler);
+}
+
+int 
+register_check(vdma_handle* handle){
+    for(int i=0; i<20; i++){
+        printf("value:0x%08x at index %d \n",((u_int32_t*)handle->vdmaVirtAddr)[i],i);
+    }
 }
 
 int 
 main(int argc, char* argv[]){
     vdma_handle vh;
     vdma_setup(&vh);
+
+    register_check(&vh);
+
+    vdma_remove(&vh);
 
     return 0;
 }
