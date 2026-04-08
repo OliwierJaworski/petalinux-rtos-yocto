7b61c3a84f2f93322b50b942ab46d5c34c99d0d5
oliwier jaworski <oliwierjaworski01@outlook.com>
2026-04-08 02:57:20 +0200

    working kernel module, first iter

--- petalinux-yocto/apps/kernel_vdma_module/vdma_overlay_driver.c
+++ petalinux-yocto/apps/kernel_vdma_module/vdma_overlay_driver.c
@@ -1,319 +1,206 @@
-
-// SPDX-License-Identifier: GPL-2.0-or-later AND Apache-2.0
-/*
- * Portions of this file:
- * Copyright (C) 2021 Xilinx, Inc
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may
- * not use these portions except in compliance with the License.
- * A copy of the License is located at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, these portions
- * of the software are distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
- * OR CONDITIONS OF ANY KIND, either express or implied. See the License
- * for the specific language governing permissions and limitations under
- * the License.
- *
- * Other portions:
- * Copyright (C) 2010-2014 Xilinx, Inc. All rights reserved.
- * Copyright (C) 2025 Oliwier Jaworski. Modifications under GPL-2.0-or-later.
- *
- * The GPL portions of this file are licensed under GPL-2.0-or-later.
- *
- * NOTE: This file contains code under both Apache 2.0 and GPL-2.0-or-later.
- * Distribution must comply with the terms of both licenses where applicable.
- */
-
-
-#include "asm-generic/errno.h"
-#include "linux/device.h"
-#include "linux/gfp_types.h"
+#include "linux/dma-direction.h"
+#include "linux/dma-mapping.h"
+#include "linux/dmaengine.h"
+#include "linux/err.h"
+#include "linux/of.h"
 #include <linux/dma/xilinx_dma.h>
-#include <linux/delay.h>
-#include <linux/init.h>
-#include <linux/kthread.h>
 #include <linux/module.h>
-#include <linux/of_dma.h>
 #include <linux/platform_device.h>
-#include <linux/random.h>
-#include <linux/slab.h>
-#include <linux/sched/task.h>
-#include <linux/wait.h>
+#include <linux/of_dma.h>
+#include <linux/io.h>
 
-#include "linux/types.h"
-#include "vdma_overlay.h"
+static unsigned int frm_cnt;
 
-#define TEST_SIZE 			1024
-
-/* set on insmod to 1 for testing */
-static unsigned internal_test = 0;
-module_param(internal_test, int, S_IRUGO);
-
-struct bd_channel{
-    struct completion cmp;
-    dma_cookie_t tx_cookie;
-    dma_addr_t dma_handle;
-    struct scatterlist sglist;
+struct vdmaOverlay_info{
+  struct platform_device *pdev;
+  struct dma_chan *tx;
+  phys_addr_t src_addr;
+  enum dma_transaction_type type; 
+  enum dma_status status;
+  struct xilinx_vdma_config cfg;
+  dma_cookie_t tx_cookie;
+  enum dma_ctrl_flags flags;
+  dma_addr_t src[3];
 };
 
-struct vdma_channel{
-    struct dma_chan *tx;
-    u32 frm_cnt;
-    u32 direction;
-    dma_addr_t bfr_phys_addr;
-
-    struct xilinx_vdma_config config;
-    enum dma_transaction_type type;
-    u8 **srcs;
-    u8 **dsts;
-    struct channel_buffer *btable_p;
-    struct bd_channel bdtable[BUFFER_COUNT];
-    u32 bdindex;
+struct vdmaoverlay_chan {
+	struct list_head node;
+	struct dma_chan *chan;
+	struct list_head threads;
 };
 
-struct vdma_overlay{
-    struct vdma_channel *chan;
-};
+static struct dma_interleaved_template xt;
 
-/* Prepare a DMA buffer to be used in a DMA transaction, submit it to the DMA engine
- * to be queued and return a cookie that can be used to track that status of the
- * transaction
+static int vdmaOverlay_configure(struct vdmaOverlay_info *vinfo){
+  set_user_nice(current, 10); 
+  
+  struct dma_device *tx_dev = vinfo->tx->device;
+  struct dma_async_tx_descriptor *txd = NULL; 
+  struct xilinx_vdma_config *cfg = &vinfo->cfg;
+/**
+ * struct xilinx_vdma_config - VDMA Configuration structure
+ * @frm_dly: Frame delay
+ * @gen_lock: Whether in gen-lock mode
+ * @master: Master that it syncs to
+ * @frm_cnt_en: Enable frame count enable
+ * @park: Whether wants to park
+ * @park_frm: Frame to park on
+ * @coalesc: Interrupt coalescing threshold
+ * @delay: Delay counter
+ * @reset: Reset Channel
+ * @ext_fsync: External Frame Sync source
+ * @vflip_en:  Vertical Flip enable
  */
-static void sync_callback(void *completion)
-{
-	/* Indicate the DMA transaction completed to allow the other
-	 * thread of control to finish processing
-	 */
-	complete(completion);
-}
-
-static void 
-start_transfer(struct vdma_channel* vc){
-    enum dma_ctrl_flags flags = DMA_CTRL_ACK | DMA_PREP_INTERRUPT;
-	struct dma_async_tx_descriptor *chan_desc;
-	struct dma_device *dma_device = vc->tx->device;
-	int bdindex = vc->bdindex;
-
-    sg_init_one(
-        &vc->bdtable[bdindex].sglist,
-        (void*)vc->bdtable[bdindex].dma_handle,
-        vc->btable_p[bdindex].length
-        );
-
-	chan_desc = dma_device->device_prep_slave_sg(vc->tx, &vc->bdtable[bdindex].sglist, 1, 
-						vc->direction, flags, NULL);
-
-    if (!chan_desc) {
-		printk(KERN_ERR "dmaengine_prep*() error\n");
-	} else {
-        chan_desc->callback = sync_callback;
-		chan_desc->callback_param = &vc->bdtable[bdindex].cmp;
-
-        init_completion(&vc->bdtable[bdindex].cmp);
-
-        vc->bdtable[bdindex].tx_cookie = dmaengine_submit(chan_desc);
-        if (dma_submit_error(vc->bdtable[bdindex].tx_cookie)) {
-            printk(KERN_ERR "Submit error\n");
-            return;
-        }
-
-        dma_async_issue_pending(vc->tx);
+/*
+  struct xilinx_vdma_config {
+	int frm_dly;
+	int gen_lock;
+	int master;
+	int frm_cnt_en;
+	int park;
+	int park_frm;
+	int coalesc;
+	int delay;
+	int reset;
+	int ext_fsync;
+	bool vflip_en;
+};*/
+  cfg->gen_lock = 0;    
+  cfg->master = 0;      
+  cfg->frm_cnt_en = 0;
+  cfg->park = 0;
+  cfg->coalesc = 0;
+  cfg->delay = 0;
+  cfg->reset = 1;  
+  xilinx_vdma_channel_set_config(vinfo->tx, cfg);
+   
+  for(int i=0;i<frm_cnt;i++){
+    pr_info("running setup loop it:%d\n",i);
+    vinfo->src[i] = dma_map_resource(tx_dev->dev, vinfo->src_addr, (1920*1080*4), DMA_TO_DEVICE,0);
+    if(dma_mapping_error(tx_dev->dev, vinfo->src[i])){
+      pr_info("failed to map dma\n");
+      continue;
     }
-}
-
-static void 
-wait_for_transfer(struct vdma_channel *vc){
-    unsigned long timeout = msecs_to_jiffies(3000);
-	enum dma_status status;
-	int bdindex = vc->bdindex;
-
-    vc->btable_p[bdindex].status = PROXY_BUSY;
-
-    timeout = wait_for_completion_timeout(&vc->bdtable[bdindex].cmp, timeout);
-	status = dma_async_is_tx_complete(vc->tx, vc->bdtable[bdindex].tx_cookie, NULL, NULL);
-
-    if (timeout == 0)  {
-        vc->btable_p[bdindex].status  = PROXY_TIMEOUT;
-        printk(KERN_ERR "DMA timed out\n");
-    } else if (status != DMA_COMPLETE) {
-        vc->btable_p[bdindex].status = PROXY_ERROR;
-        printk(KERN_ERR "DMA returned completion callback status of: %s\n",
-                status == DMA_ERROR ? "error" : "in progress");
-    } else
-        vc->btable_p[bdindex].status = PROXY_NO_ERROR;
-}
-
-static int
-test( struct platform_device *pdev ){
-    struct vdma_overlay *vo;
-
-    vo = (struct vdma_overlay*)dev_get_drvdata(&pdev->dev);
-    if(IS_ERR_OR_NULL(vo)){
-        dev_err(&pdev->dev, "Could not allocate for < struct vdma_overlay >");
-        if(!vo){
-            return -ENODATA;
-        } else { 
-            return PTR_ERR(vo);
-        }
-    }
-    struct vdma_channel *vc= vo->chan;
-
-    vc->btable_p[0].length = TEST_SIZE;
-    vc->bdindex = 0;
-
-    /* fill in buffer 0 for testing with white screen */
-    for(int i=0; i< BUFFER_SIZE/sizeof(unsigned int); i++){
-        vc->btable_p[0].buffer[i] = 0xFF;
-    }
-
-    start_transfer(vc);
-    wait_for_transfer(vc);
-    return 0;
-}
-
-static int 
-vdma_channels_setup( struct platform_device *pdev ){
-    struct vdma_overlay *vo;
-
-    vo = (struct vdma_overlay*)dev_get_drvdata(&pdev->dev);
-    if(IS_ERR_OR_NULL(vo)){
-        dev_err(&pdev->dev, "Could not allocate for < struct vdma_overlay >");
-        if(!vo){
-            return -ENODATA;
-        } else { 
-            return PTR_ERR(vo);
-        }
-    }
-    struct vdma_channel *vc= vo->chan;
-
-    vc->btable_p = (struct channel_buffer *) 
-        dmam_alloc_coherent(&pdev->dev, sizeof(struct channel_buffer) * vc->frm_cnt,
-                &vc->bfr_phys_addr, GFP_KERNEL | GFP_DMA);
-    if (IS_ERR_OR_NULL(vc->btable_p)) {
-        dev_err(&pdev->dev, "DMA allocation error\n");
-        if(!vc->btable_p) {
-            return -ENOMEM;
-        } else {
-            return PTR_ERR(vc->btable_p);
-        }
-    }
-    printk(KERN_INFO "Allocating memory, virtual address: %px physical address: %px\n",
-            vc->btable_p, (void *)vc->bfr_phys_addr);
-
-    for(int bd =0; bd < BUFFER_COUNT; bd++){
-        vc->bdtable[bd].dma_handle = (dma_addr_t)(vc->bfr_phys_addr +
-                (sizeof(struct channel_buffer)*bd) + offsetof(struct channel_buffer, buffer));
-        printk(KERN_INFO "\t%d: %px\n",bd,(void*)vc->bdtable[bd].dma_handle);
+    xt.src_start = vinfo->src[i];
+    xt.dir = DMA_MEM_TO_DEV;
+    xt.numf = 1080;
+    xt.sgl[0].size = (1920*4);
+    xt.sgl[0].icg = 0;
+    xt.frame_size = 1;
+    txd = tx_dev->device_prep_interleaved_dma(vinfo->tx, &xt, vinfo->flags);
+    //txd = tx_dev->device_prep_dma_cyclic(vinfo->tx, vinfo->src[i], (7680*1080), (7680*1080),DMA_MEM_TO_DEV,vinfo->flags); 
+    if(IS_ERR(txd)){
+      dma_release_channel(vinfo->tx);
+      printk("Error: %ld, in device_prep_dma_cyclic", PTR_ERR(txd));
+      return PTR_ERR(txd);
     } 
 
-    /*init so no garbage*/
-    vc->bdindex = 0;
-    vc->direction = DMA_MEM_TO_DEV;
-
-    return 0;
-};
-
-    static int 
-vdma_overlay_probe( struct platform_device *pdev )
-{
-    struct vdma_overlay *vo;
-    struct vdma_channel *vchan;
-    int rc;
-
-    /* allocate struct vdma_overlay*/
-    vo = (struct vdma_overlay *) devm_kmalloc(&pdev->dev, sizeof(struct vdma_overlay), GFP_KERNEL);
-    if(IS_ERR_OR_NULL(vo)){
-        dev_err(&pdev->dev, "Could not allocate for < struct vdma_overlay >");
-        if(!vo){
-            return -ENOMEM;
-        } else { 
-            return PTR_ERR(vo);
-        }
+    //dmaengine_submit() will not start the DMA operation, it merely adds it to the pending queue.
+    //vinfo->tx_cookie = txd->tx_submit(txd);
+    vinfo->tx_cookie = txd->tx_submit(txd);
+  }
+ 
+  if(!txd){
+    for(int i=0;i<frm_cnt;i++){
+      dma_unmap_resource(tx_dev->dev, vinfo->src[i], 1920*1080*4, DMA_TO_DEVICE, 0);
     }
+    pr_warn("error during prep?\n");
+  } 
 
-    /* allocate allocate vdma_channel*/
-    vchan = (struct vdma_channel *) devm_kmalloc(&pdev->dev, sizeof(struct vdma_channel ), GFP_KERNEL);
-    if(IS_ERR_OR_NULL(vchan)){
-        dev_err(&pdev->dev, "Could not allocate for < struct vdma_channel >");
-        if(!vchan){
-            return -ENOMEM;
-        } else { 
-            return PTR_ERR(vchan);
-        }
-    }
-
-    vo->chan = vchan; 
-
-    /*instead of globals*/ 
-    dev_set_drvdata(&pdev->dev,vo);
-
-    rc = of_property_read_u32(pdev->dev.of_node,
-            "xlnx,num-fstores", &vchan->frm_cnt);
-    if (rc < 0) {
-        printk(KERN_ERR "vdma_overlay: missing xlnx,num-fstores property\n");
-        return rc;
-    }
-
-    vchan->tx = dma_request_chan(&pdev->dev, "vdma0");
-    if (IS_ERR(vchan->tx)) {
-        rc = PTR_ERR(vchan->tx);
-        if (rc != -EPROBE_DEFER)
-            pr_err("vdma_overlay: No Tx channel\n");
-        return rc;
-    }
-
-    if (rc) {
-        pr_err("vdma_overlay: Unable to add channels\n");
-        dma_release_channel(vchan->tx);
-        return rc;
-    } 
-    vdma_channels_setup(pdev);
-
-    if (internal_test)
-        test(pdev);
-    return 0;
+  dma_async_issue_pending(vinfo->tx);
+  pr_info("oj_vdma_overlay configured\n");
+  return 0;
 }
 
-    static int 
-vdma_overlay_remove(struct platform_device *pdev)
-{
-    struct device *dev = &pdev->dev;
-    struct vdma_overlay *lp = dev_get_drvdata(dev);
+static int vdmaOverlay_probe(struct platform_device *pdev){
+  dev_info(&pdev->dev, "OJ -  VDMA overlay Driver Probed!!\n");
 
-    printk(KERN_INFO "dma_proxy module exited\n");
+  int err;
+  struct vdmaOverlay_info *vinfo = kmalloc(sizeof(struct vdmaOverlay_info), GFP_KERNEL);
+  if (!vinfo)
+    return -ENOMEM;
+  memset(&vinfo->cfg, 0, sizeof(struct xilinx_vdma_config));
+  vinfo->pdev = pdev;
+  vinfo->src_addr = (phys_addr_t)0x10000000;
+  vinfo->flags = DMA_CTRL_ACK | DMA_PREP_INTERRUPT;
 
-    /* Take care of the DMA channels and any buffers allocated
-     * for the DMA transfers. The DMA buffers are using managed
-     * memory such that it's automatically done.
-     */
-    if (lp->chan->tx) {
-        lp->chan->tx->device->device_terminate_all(lp->chan->tx);
-        dma_release_channel(lp->chan->tx);
+  // search whether it has given property
+  err = of_property_read_u32(pdev->dev.of_node, "xlnx,num-fstores", &frm_cnt);
+
+  if (err < 0){
+    pr_err("oj-vdmaoverlay: missing xlnx,num-fstores property\n"); 
+    kfree(vinfo);
+    return err;
+  }
+  dev_info(&pdev->dev, "oj-vdmaoverlay: xlnx,num-fstores:%d\n",frm_cnt);
+  
+  vinfo->tx= dma_request_chan(&pdev->dev, "vdma0");
+  if(IS_ERR(vinfo->tx)){
+    err = PTR_ERR(vinfo->tx);
+    if(err != -EPROBE_DEFER){
+      kfree(vinfo);
+      pr_err("oj-vdmaoverlay: No Tx channel\n");
     }
-    return 0;
+    return err;
+  }
+  
+  //here will go code configuring everything
+  
+  err = vdmaOverlay_configure(vinfo);
+  if(err){// free dma resource
+    kfree(vinfo);
+    pr_err("oj-vdmaoverlay: failed to add channels\n");
+    goto free_tx;
+  }
+  platform_set_drvdata(pdev, vinfo);
+  return 0;
+
+  free_tx:
+    dma_release_channel(vinfo->tx);
+  
+  return err;
 }
 
-static const struct of_device_id vdma_overlay_of_ids[]={
-    {.compatible = "xlnx,vdma-overlay-1.0",},
-    {}
+static int vdmaOverlay_remove(struct platform_device *pdev){
+  dev_info(&pdev->dev, "OJ -  VDMA overlay Driver Removed!!\n");
+  
+  //struct dma_chan *tx;
+  struct vdmaOverlay_info *vinfo = platform_get_drvdata(pdev);
+    if (!vinfo)
+        return 0;
+  
+  //must be called before so termination can be done succesfully
+  dmaengine_synchronize(vinfo->tx);
+  dmaengine_terminate_async(vinfo->tx);
+  for(int i=0;i<frm_cnt;i++)
+    dma_unmap_resource(vinfo->tx->device->dev, vinfo->src[i], 1920*1080*4, DMA_TO_DEVICE, 0);
+  //release exclusive access to dma channel
+  dma_release_channel(vinfo->tx);
+  kfree(vinfo);
+
+  return 0;
+}
+
+static const struct of_device_id oj_vdmaoverlay_of_ids[] = {
+  { .compatible = "xlnx,vdma-overlay-1.0",},
+  {}
 };
 
-static struct platform_driver vdma_connect_driver = {
-    .probe      = vdma_overlay_probe,
-    .remove     = vdma_overlay_remove,
-    .driver     = {
-        .name     = "vdma_overlay",
-        .owner    = THIS_MODULE,
-        .of_match_table = vdma_overlay_of_ids,
-    },
+static struct platform_driver oj_vdmaoverlay_driver = {
+  .driver = {
+    .name = "oj_vdma_overlay",
+    .owner = THIS_MODULE,
+    .of_match_table = oj_vdmaoverlay_of_ids,
+  },
+  .probe =vdmaOverlay_probe, 
+  .remove =vdmaOverlay_remove, 
 };
 
-// for driver that dont do anything special in module init/exit | eliminates alot of boilerplate
-module_platform_driver(vdma_connect_driver);
+module_platform_driver(oj_vdmaoverlay_driver)
 
-MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Oliwier Jaworski");
-MODULE_DESCRIPTION("Simple GPIO driver to interface ZYNQ PS GPIO");
-MODULE_VERSION("0.1");
+MODULE_DESCRIPTION("simple vdma-framebuffer overlay");
+MODULE_VERSION("0.01");
+MODULE_LICENSE("GPL v2");
+
+
